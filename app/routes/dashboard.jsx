8d36aab9533751aea008f88c31de5144d939504f
Daniel Gornick <daniel.g.gornick@gmail.com>
2025-11-17 19:43:58 -0500

    Modified dashboard.css

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -5,92 +5,92 @@ export const links = () => [
 export default function Dashboard() {
   return (
     <>
-      <nav class="navbar">
-        <div class="logo">Campus Lost & Found</div>
-        <input type="text" class="search-bar" placeholder="Search items" />
-        <div class="nav-links">
-          <button class="nav-btn">Messages</button>
-          <button class="nav-btn">Profile</button>
+      <nav className="navbar">
+        <div className="logo">Campus Lost & Found</div>
+        <input type="text" className="search-bar" placeholder="Search items" />
+        <div className="nav-links">
+          <button className="nav-btn">Messages</button>
+          <button className="nav-btn">Profile</button>
         </div>
-        <div class="profile-picture">
+        <div className="profile-picture">
           <img src="/images/profile.png" />
         </div>
       </nav>
 
-      <div class="feed">
-        <div class="item">
+      <div className="feed">
+        <div className="item">
           <img src="/images/image.png" alt="Lost Item" />
-          <div class="info">
-            <div class="title">Black Backpack</div>
-            <div class="location">Hillman Library</div>
-            <button class="report-button">Report Found</button>
+          <div className="info">
+            <div className="title">Black Backpack</div>
+            <div className="location">Hillman Library</div>
+            <button className="report-button">Report Found</button>
           </div>
         </div>
 
-        <div class="item">
+        <div className="item">
           <img src="/images/background2.jpg" alt="Lost Item" />
-          <div class="info">
-            <div class="title">AirPods Case</div>
-            <div class="location">Cathedral 3rd Floor</div>
-            <button class="report-button">Report Found</button>
+          <div className="info">
+            <div className="title">AirPods Case</div>
+            <div className="location">Cathedral 3rd Floor</div>
+            <button className="report-button">Report Found</button>
           </div>
         </div>
 
-        <div class="item">
+        <div className="item">
           <img src="/images/background2.jpg" alt="Lost Item" />
-          <div class="info">
-            <div class="title">Pitt ID</div>
-            <div class="location">Market</div>
-            <button class="report-button">Report Found</button>
+          <div className="info">
+            <div className="title">Pitt ID</div>
+            <div className="location">Market</div>
+            <button className="report-button">Report Found</button>
           </div>
         </div>
-        <div class="item">
+        <div className="item">
           <img src="/images/background2.jpg" alt="Lost Item" />
-          <div class="info">
-            <div class="title">Blue Hydro Flask</div>
-            <div class="location">Benedum</div>
-            <button class="report-button">Report Found</button>
-          </div>
-        </div>
-
-        <div class="item">
-          <img src="/images/background2.jpg" alt="Lost Item" />
-          <div class="info">
-            <div class="title">Apple Watch Series 7</div>
-            <div class="location">Rec Center Locker Room</div>
-            <button class="report-button">Report Found</button>
+          <div className="info">
+            <div className="title">Blue Hydro Flask</div>
+            <div className="location">Benedum</div>
+            <button className="report-button">Report Found</button>
           </div>
         </div>
 
-        <div class="item">
+        <div className="item">
           <img src="/images/background2.jpg" alt="Lost Item" />
-          <div class="info">
-            <div class="title">Set of Keys with Panther Keychain</div>
-            <div class="location">William Pitt Union</div>
-            <button class="report-button">Report Found</button>
+          <div className="info">
+            <div className="title">Apple Watch Series 7</div>
+            <div className="location">Rec Center Locker Room</div>
+            <button className="report-button">Report Found</button>
           </div>
         </div>
 
-        <div class="item">
+        <div className="item">
           <img src="/images/background2.jpg" alt="Lost Item" />
-          <div class="info">
-            <div class="title">TI-84+ Calculator</div>
-            <div class="location">Thackeray Hall 213</div>
-            <button class="report-button">Report Found</button>
+          <div className="info">
+            <div className="title">Set of Keys with Panther Keychain</div>
+            <div className="location">William Pitt Union</div>
+            <button className="report-button">Report Found</button>
           </div>
         </div>
 
-        <div class="item">
+        <div className="item">
           <img src="/images/background2.jpg" alt="Lost Item" />
-          <div class="info">
-            <div class="title">Golden Earrings</div>
-            <div class="location">Cathy Ground Floor Bathroom</div>
-            <button class="report-button">Report Found</button>
+          <div className="info">
+            <div className="title">TI-84+ Calculator</div>
+            <div className="location">Thackeray Hall 213</div>
+            <button className="report-button">Report Found</button>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="/images/background2.jpg" alt="Lost Item" />
+          <div className="info">
+            <div className="title">Golden Earrings</div>
+            <div className="location">Cathy Ground Floor Bathroom</div>
+            <button className="report-button">Report Found</button>
           </div>
         </div>
 
       </div>
-      <button class="add-button">+</button>
+      <button className="add-button">+</button>
     </>
   );
 }
