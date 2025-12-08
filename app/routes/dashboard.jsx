2419a6ac8cf663d0e6403e90dcdd8db7fddb138f
gregbetron <gregb4789@gmail.com>
2025-12-07 19:02:49 -0500

    Added lost items  to database and updated dashboard to display items from the database.

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -1,9 +1,17 @@
 import LostItem from "../components/LostItem";
+import { useLoaderData } from "react-router";
+
 export const links = () => [
   { rel: "stylesheet", href: "/app/styles/dashboard.css" },
 ];
+export const loader = async () => {
+  const itemData = await fetch("http://localhost:3001/items");
+  return itemData.json();
+};
 
 export default function Dashboard() {
+  const items = useLoaderData();
+
   return (
     <>
       <nav className="navbar">
@@ -19,54 +27,11 @@ export default function Dashboard() {
       </nav>
 
       <div className="feed">
-        <LostItem
-          image="/images/backpack.png"
-          title="Black Backpack"
-          location="Hillman Library"
-          date="12/21/2025"
-        />
-        <LostItem
-          image="/images/airpods.png"
-          title="AirPods Case"
-          location="Cathedral 3rd Floor"
-          date="12/18/2025"
-        />
-        <LostItem
-          image="/images/bottle.png"
-          title="Pitt Water Bottle"
-          location="Market"
-          date="12/15/2025"
-        />
-        <LostItem
-          image="/images/hydroflask.png"
-          title="Blue Hydro Flask"
-          location="Benedum"
-          date="12/11/2025"
-        />
-        <LostItem
-          image="/images/watch.png"
-          title="Apple Watch Series 7"
-          location="Rec Center Locker Room"
-          date="12/20/2025"
-        />
-        <LostItem
-          image="/images/keychain.png"
-          title="Pitt Panther Keychain"
-          location="William Pitt Union"
-          date="12/19/2025"
-        />
-        <LostItem
-          image="/images/calc.png"
-          title="TI-84+ Calculator"
-          location="Thackeray Hall 213"
-          date="12/16/2025"
-        />
-        <LostItem
-          image="/images/earring.png"
-          title="Golden Earrings"
-          location="Cathy Ground Floor Bathroom"
-          date="12/14/2025"
-        />
+        {items.map((item) => (
+          <LostItem
+            id = {item.id}{...item}
+          />
+        ))}
       </div>
 
       <button className="add-button">+</button>
